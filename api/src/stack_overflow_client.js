f43189ed84580d5733d1ebb6355351ae7827d681
Cesar Gonzalez <cegonse@gmail.com>
2020-07-25 16:51:34 +0200

    Test

--- api/src/stack_overflow_client.js
+++ api/src/stack_overflow_client.js
@@ -1,7 +1,7 @@
 require('isomorphic-fetch')
 const xmlParser = require('fast-xml-parser')
 
-const STACK_OVERFLOW_RSS_FEED_URL = 'https://stackoverflow.com/jobs/feed?q=frontend&l=valencia&u=Km&d=20'
+const STACK_OVERFLOW_RSS_FEED_URL = 'https://stackoverflow.com/jobs/feed'
 
 
 class StackOverflowClient {
